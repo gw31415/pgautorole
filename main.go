c90fe04e032f997a385a5b44e1f62d07d47dfe05
gw31415 <gw31415@amas.dev>
2024-06-18 18:25:16 +0900

    Newbie Manager

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/gw31415/pgautorole/newbie"
+	"github.com/robfig/cron/v3"
+)
+
+var (
+	// デバッグモード
+	DEBUG_MODE = os.Getenv("DEBUG_MODE")
+
+	// Discordのトークン
+	DISCORD_TOKEN = os.Getenv("DISCORD_TOKEN")
+
+	// 一般会員のロールID
+	MEMBER_ROLE_ID = os.Getenv("MEMBER_ROLE_ID")
+	// 新規会員のロールID
+	NEWBIE_ROLE_ID = os.Getenv("NEWBIE_ROLE_ID")
+	// 新規会員のロールをフィルターするスケジュール
+	NEWBIE_FILTERING_CRON = os.Getenv("NEWBIE_FILTERING_CRON")
+	// 新規会員のロールの有効期間
+	NEWBIE_MAX_DURATION, _ = time.ParseDuration(os.Getenv("NEWBIE_MAX_DURATION"))
+)
+
+func main() {
+	if len(os.Getenv("DEBUG_MODE")) > 0 {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+		slog.Debug("Debug mode")
+	}
+
+	// 環境変数のチェック
+	if DISCORD_TOKEN == "" || MEMBER_ROLE_ID == "" || NEWBIE_ROLE_ID == "" || NEWBIE_FILTERING_CRON == "" || NEWBIE_MAX_DURATION == 0 {
+		slog.Error("Please set environment variables")
+		return
+	}
+
+	// Discordセッションの作成
+	discord, err := discordgo.New("Bot " + DISCORD_TOKEN)
+	if err != nil {
+		slog.Error("Error creating Discord session:", err)
+		return
+	}
+	discord.Identify.Intents = discordgo.IntentsGuildMembers
+
+	// cronの初期化
+	cr := cron.New()
+
+	// Setup NewbieRoleManager
+	slog.Info("Setting up NewbieRoleManager", "MEMBER_ROLE_ID", MEMBER_ROLE_ID, "NEWBIE_ROLE_ID", NEWBIE_ROLE_ID, "NEWBIE_MAX_DURATION", NEWBIE_MAX_DURATION)
+	newbiemanager := newbie.NewNewbieManager(NEWBIE_ROLE_ID, MEMBER_ROLE_ID, NEWBIE_MAX_DURATION)
+	discord.AddHandler(newbiemanager.MemberRoleUpdateHandler)
+	_, err = cr.AddFunc(NEWBIE_FILTERING_CRON, func() {
+		slog.Info("Filtering newbie roles")
+		newbiemanager.FilterNewbieRoles(discord)
+	})
+	if err != nil {
+		slog.Error("Error adding cron job:", err)
+		return
+	}
+
+	// Discordセッションのオープン
+	slog.Info("Opening discord connection")
+	err = discord.Open()
+	if err != nil {
+		slog.Error("Error opening discord connection:", err)
+		return
+	}
+	defer discord.Close()
+
+	// cronの開始
+	slog.Info("Starting cron")
+	go cr.Run()
+	defer cr.Stop()
+
+	// 終了シグナルの待機
+	slog.Info("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	slog.Info("Shutting down...")
+}
---
+++ newbie/newbie.go
@@ -0,0 +1,95 @@
+package newbie
+
+import (
+	"log/slog"
+	"slices"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type NewbieManager interface {
+	// 会員ロール変化時に新規会員ロールを操作するハンドラ
+	MemberRoleUpdateHandler(s *discordgo.Session, m *discordgo.GuildMemberUpdate)
+	// 新規会員ロールを持っているメンバーから新規会員ロールを削除
+	FilterNewbieRoles(s *discordgo.Session)
+}
+
+type newbieManager struct {
+	newbieRoleID   string
+	memberRoleID   string
+	newbieDuration time.Duration
+}
+
+func NewNewbieManager(newbieRoleID, memberRoleID string, newbieDuration time.Duration) NewbieManager {
+	return &newbieManager{
+		newbieRoleID:   newbieRoleID,
+		memberRoleID:   memberRoleID,
+		newbieDuration: newbieDuration,
+	}
+}
+
+// userが新規会員に該当するかどうか
+func (n *newbieManager) checkNewbie(s *discordgo.Session, member *discordgo.Member) (bool, error) {
+	age := time.Since(member.JoinedAt)
+	// 会員でない場合は新規会員ではない
+	if !slices.Contains(member.Roles, n.memberRoleID) {
+		return false, nil
+	}
+	// JoinedAtからNEWBIE_DURAITON経っていない新規会員
+	return age < n.newbieDuration, nil
+}
+
+func (n *newbieManager) MemberRoleUpdateHandler(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
+	// 会員ロールが付与された時
+	if m.BeforeUpdate != nil && slices.Contains(m.Roles, n.memberRoleID) && (m.BeforeUpdate.Roles == nil || !slices.Contains(m.BeforeUpdate.Roles, n.memberRoleID)) {
+		isNewbie, err := n.checkNewbie(s, m.Member)
+		if err == nil && isNewbie {
+			slog.Info("Add newbie role", "m.User.ID", m.User.ID)
+			s.GuildMemberRoleAdd(m.GuildID, m.User.ID, n.newbieRoleID)
+		}
+	}
+	// 新規会員ロールが手動付与された時
+	if m.BeforeUpdate != nil && slices.Contains(m.Roles, n.newbieRoleID) && (m.BeforeUpdate.Roles == nil || !slices.Contains(m.BeforeUpdate.Roles, n.newbieRoleID)) {
+		isNewbie, err := n.checkNewbie(s, m.Member)
+		if err == nil && !isNewbie {
+			// 条件にあてはまらない場合キャンセル
+			slog.Info("Refuse newbie role", "m.User.ID", m.User.ID)
+			s.GuildMemberRoleRemove(m.GuildID, m.User.ID, n.newbieRoleID)
+		}
+	}
+}
+
+const MEMBERS_PER_REQUEST = 1000
+
+func (n *newbieManager) FilterNewbieRoles(s *discordgo.Session) {
+	for _, guild := range s.State.Guilds {
+		after := ""
+		for {
+			// MEMBER_PER_REQUESTずつメンバーを取得
+			m, err := s.GuildMembers(guild.ID, after, MEMBERS_PER_REQUEST)
+			// メンバーが取得できなかった場合は終了
+			if err != nil || len(m) == 0 {
+				break
+			}
+
+			// 全メンバーに対して処理
+			for _, member := range m {
+				// 会員ロールと新規会員ロールを持っている場合
+				if slices.Contains(member.Roles, n.memberRoleID) && slices.Contains(member.Roles, n.newbieRoleID) {
+					// 新規会員でない場合は新規会員ロールを削除
+					isNewbie, err := n.checkNewbie(s, member)
+					if err == nil && !isNewbie {
+						slog.Info("Remove newbie role", "member.User.ID", member.User.ID)
+						err := s.GuildMemberRoleRemove(guild.ID, member.User.ID, n.newbieRoleID)
+						if err != nil {
+							slog.Error("Failed to remove newbie role", err)
+						}
+					}
+				}
+			}
+
+			after = m[len(m)-1].User.ID
+		}
+	}
+}
